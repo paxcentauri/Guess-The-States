f3723b3ac767c7ef229dee2cec1847cce85d8910
Shikhar Chaurasia <81959120+paxcentauri@users.noreply.github.com>
2022-01-16 15:47:02 +0400

    Uploaded the main program along with the CSV files
    50_states.csv has the list of states and their (x,y) locations as part of the turtle map
    states_to_learn.csv is generated at the end of execution, including a list of all the states that the user did not guess.
    Type exit when you don't want guess anymore.

---
+++ main.py
@@ -0,0 +1,61 @@
+import turtle
+import pandas
+
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+screen.addshape("blank_states_img.gif")
+turtle.shape("blank_states_img.gif")
+missing_states = []
+
+
+def write_on_map(ans, x, y):
+    state = turtle.Turtle()
+    state.penup()
+    state.hideturtle()
+    state.goto(x, y)
+    state.write(ans)
+
+
+game_is_on = True
+user_score = 0
+states_data = pandas.read_csv("50_states.csv")
+states_list = states_data["state"].to_list()
+print(states_list)
+gtitle = ""
+guessed_list = []
+
+
+def generate_missing():
+    for state in states_list:
+        if state not in guessed_list:
+            missing_states.append(state)
+    missing_dict = {
+        "State Names": missing_states
+    }
+    missing_data = pandas.DataFrame(missing_dict)
+    missing_data.to_csv("states_to_learn.csv")
+
+
+while game_is_on is True:
+    if len(guessed_list) == 0:
+        gtitle = "Guess the States"
+    else:
+        gtitle = f"Guessed {user_score}/50"
+    answer = screen.textinput(title=gtitle, prompt="Guess a US State name.")
+    answer = answer.title()
+    if answer == "Exit":
+        generate_missing()
+        game_is_on = False
+
+    if answer in states_list and answer not in guessed_list:
+        guessed_list.append(answer)
+        user_score += 1
+        rec = states_data[states_data.state == answer]
+        ans_x = float(rec.x)
+        ans_y = float(rec.y)
+        write_on_map(answer, ans_x, ans_y)
+    if user_score == 50:
+        game_is_on = False
+
+
+
